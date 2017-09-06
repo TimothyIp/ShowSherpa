936ff0a59cb71d184bd051d54e83873c14cf5ff7
Timothy Ip <timothy.ip4@gmail.com>
2017-09-06 16:37:25 -0400

    Updated Files

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { 
+    BrowserRouter as Router, 
+    Route, Link, NavLink } from 'react-router-dom';
+import { ajax } from 'jquery'
+
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                Hello React!
+              <h1>All set and ready.</h1>
             </div>
         )
     }
