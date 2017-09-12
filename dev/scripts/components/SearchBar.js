27ae226ba1cee3af31a4e112590d5083bab3cb2b
Timothy Ip <timothy.ip4@gmail.com>
2017-09-12 18:20:33 -0400

    tweak

--- dev/scripts/components/SearchBar.js
+++ dev/scripts/components/SearchBar.js
@@ -5,7 +5,7 @@ class SearchBar extends React.Component {
 	render(){
 		return (
 				<form className="searchbar__form"onSubmit={this.props.searchShows}>
-					<input onChange={this.props.handleChange} name="searchedShows" type="text" placeholder="Search Tv Shows..." />
+					<input onChange={this.props.handleChange} name="searchedShows" type="text" placeholder="Search TV Shows..." />
 				</form>
 			)
 	}
